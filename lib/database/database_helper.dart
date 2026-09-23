40d024fd658ffa5c18316f63e9f8ad09bc35f582
João Pedro Moreira Martins de Sousa <joao.sousa@fasec.edu.br>
2026-09-23 20:18:09 -0300

    chore: auto-sync 2026-09-23 20:18:09

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,2 +1,20 @@
 import 'package:path/path.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper()._init();
+
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if(_database != null){
+       return _database!;
+    }
+  
+  _database = await _initDB('tarefas.db');
+  return _database!;
+  }
+
+}
